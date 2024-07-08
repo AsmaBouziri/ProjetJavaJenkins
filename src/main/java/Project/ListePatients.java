58fbe1a4a9984a5816fe604a32b578668b627feb
asmabouziri <asmabouziri299@gmail.com>
2024-07-08 09:42:46 +0100

    v2

--- src/main/java/Project/ListePatients.java
+++ src/main/java/Project/ListePatients.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
-importmain.java. Project.RechercherPatient;
+import main.java. Project.RechercherPatient;
 
 public class ListePatients extends JFrame implements ActionListener {
     
