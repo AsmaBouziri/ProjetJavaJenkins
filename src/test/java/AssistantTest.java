bf6107b7d48c4d9fe725e1599da0f049e5008c5d
asmabouziri <asmabouziri299@gmail.com>
2024-07-09 10:20:00 +0100

    v6

---
+++ src/test/java/AssistantTest.java
@@ -0,0 +1,13 @@
+// package test.java;
+
+// import static org.junit.Assert.assertTrue;
+
+// import org.junit.Test;
+
+// public class AssistantTest {
+
+//     @Test
+//     public void contextLoads() {
+//         assertTrue(true);
+//     }
+// }
