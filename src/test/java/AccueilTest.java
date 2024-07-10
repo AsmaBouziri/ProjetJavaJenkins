a9910d5a11cce8e397d488fddd4a7c9cb9730cbd
asmabouziri <asmabouziri299@gmail.com>
2024-07-10 10:54:52 +0100

    v9

---
+++ src/test/java/AccueilTest.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class AccueilTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
