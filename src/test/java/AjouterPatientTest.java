6bbd5bcae6a9f14769cfcac7fb70c269effba7e4
asmabouziri <asmabouziri299@gmail.com>
2024-07-16 11:30:51 +0100

    test classes

--- src/test/java/AjouterPatientTest.java
+++ src/test/java/AjouterPatientTest.java
@@ -7,7 +7,6 @@ import com.mongodb.client.MongoDatabase;
 import main.java.AjouterPatient;
 import org.bson.Document;
 import org.mockito.Mockito;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class AjouterPatientTest {
@@ -23,7 +22,6 @@ public class AjouterPatientTest {
 
         MongoClient mockMongoClient = Mockito.mock(MongoClient.class);
         database = Mockito.mock(MongoDatabase.class);
-        patientsCollection = Mockito.mock(MongoCollection.class);
         Mockito.when(mockMongoClient.getDatabase("CabinetDent")).thenReturn(database);
         Mockito.when(database.getCollection("Patient")).thenReturn(patientsCollection);
 
@@ -39,31 +37,6 @@ public class AjouterPatientTest {
         Mockito.verifyNoMoreInteractions(patientsCollection);
     }
 
-    @Test
-    public void testAddPatientWithValidData() {
-        frame.getNomTextField().setText("Dupont");
-        frame.getPrenomTextField().setText("Jean");
-        frame.getCinTextField().setText("123456789");
-        frame.getAdresseTextField().setText("1 Rue du Soleil");
-        frame.getProfessionTextField().setText("Ingénieur");
-        frame.getTelTextField().setText("0123456789");
-        frame.getHommeRadioButton().setSelected(true);
-
-        ActionEvent event = new ActionEvent(frame.getEnregistrerButton(), ActionEvent.ACTION_PERFORMED, "");
-        for (ActionListener listener : frame.getEnregistrerButton().getActionListeners()) {
-            listener.actionPerformed(event);
-        }
-
-        Document expectedPatientDocument = new Document()
-                .append("nom", "Dupont")
-                .append("prenom", "Jean")
-                .append("cin", "123456789")
-                .append("adresse", "1 Rue du Soleil")
-                .append("profession", "Ingénieur")
-                .append("tel", "0123456789")
-                .append("sexe", "Homme")
-                .append("dateNaissance", "");
-        Mockito.verify(patientsCollection).insertOne(expectedPatientDocument);
-    }
+  
 
 }
