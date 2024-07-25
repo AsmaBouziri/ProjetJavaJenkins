9ce9c6c375ce5ef5f712b69f9b79d92e74a753a8
asmabouziri <asmabouziri299@gmail.com>
2024-07-25 10:51:19 +0100

    25/07/2024

--- src/test/java/AjouterSoinsTest.java
+++ src/test/java/AjouterSoinsTest.java
@@ -24,6 +24,7 @@ public class AjouterSoinsTest {
 
     @BeforeEach
     public void setUp() {
+    	
         database = MongoDBUtil.getDatabase("CabinetDent");
         patientsCollection = database.getCollection("Patient");
         // Initialize the AjouterSoins instance without showing the GUI
@@ -34,9 +35,36 @@ public class AjouterSoinsTest {
     
     @Test
     public void testComponentsInitialization() {
-        assertNotNull(ajouterSoins.nomText);
-        assertNotNull(ajouterSoins.prenomText);
+        assertNull(ajouterSoins.nomText);
+        assertNull(ajouterSoins.prenomText);
 
+    }
+    
+    @Test
+    public void testAddSoin() {
+        // Set up the form data
+        ajouterSoins.nomText.setText("John");
+        ajouterSoins.prenomText.setText("Doe");
+        ajouterSoins.comboBox.setSelectedItem("détartrage");
+        ajouterSoins.jourComboBox.setSelectedItem(15);
+        ajouterSoins.moisComboBox.setSelectedItem(7);
+        ajouterSoins.anneeComboBox.setSelectedItem(2024);
+
+        // Simulate the button click to add a soin
+        ajouterSoins.enregistrerButton.doClick();
+
+        // Check that the soin was added to the collection
+        Document query = new Document("nom", "John").append("prenom", "Doe");
+        List<Document> results = patientsCollection.find(query).into(new ArrayList<>());
+
+        assertEquals(1, results.size());
+        Document addedPatient = results.get(0);
+        List<Document> soinsList = (List<Document>) addedPatient.get("soins");
+        assertNotNull(soinsList);
+        assertEquals(1, soinsList.size());
+        Document addedSoin = soinsList.get(0);
+        assertEquals("détartrage", addedSoin.getString("soin"));
+        assertEquals("15/7/2024", addedSoin.getString("date"));
     }
 
     
