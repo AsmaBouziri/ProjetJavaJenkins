2a18d480964d02fb692694af6528d04b19488b07
asmabouziri <asmabouziri299@gmail.com>
2024-07-25 09:40:29 +0100

    25/07/2024

--- src/test/java/AjouterSoinsTest.java
+++ src/test/java/AjouterSoinsTest.java
@@ -37,88 +37,39 @@ public class AjouterSoinsTest {
         collection = database.getCollection("Patient");
     }
 
-    @Test
-    public void testEnregistrerButtonInsertNewPatient() {
-        // Configure the test data
-        ajouterSoins.nomText.setText("Dupont");
-        ajouterSoins.prenomText.setText("Jean");
+//    @Test
+//    public void testEnregistrerButtonInsertNewPatient() {
+//        // Configure the test data
+//        ajouterSoins.nomText.setText("Dupont");
+//        ajouterSoins.prenomText.setText("Jean");
+//
+//        JComboBox<String> comboBox = (JComboBox<String>) getPrivateField(ajouterSoins, "comboBox");
+//        comboBox.setSelectedItem("détartrage");
+//
+//        JComboBox<Integer> jourComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "jourComboBox");
+//        jourComboBox.setSelectedItem(15);
+//
+//        JComboBox<Integer> moisComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "moisComboBox");
+//        moisComboBox.setSelectedItem(8);
+//
+//        JComboBox<Integer> anneeComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "anneeComboBox");
+//        anneeComboBox.setSelectedItem(2024);
+//
+//        JButton enregistrerButton = (JButton) getPrivateField(ajouterSoins, "enregistrerButton");
+//        enregistrerButton.doClick();
+//
+//        // Verify the interaction with MongoDB
+//        Document found = collection.find(new Document("nom", "Dupont")).first();
+//        assertNotNull(found, "Patient 'Dupont' should be found");
+//        assertEquals("Jean", found.getString("prenom"));
+//        
+//        List<Document> soinsList = (List<Document>) found.get("soins");
+//        assertNotNull(soinsList);
+//        assertEquals(1, soinsList.size());
+//
+//        Document soinDocument = soinsList.get(0);
+//        assertEquals("détartrage", soinDocument.getString("soin"));
+//        assertEquals("15/8/2024", soinDocument.getString("date"));
+//    }
 
-        JComboBox<String> comboBox = (JComboBox<String>) getPrivateField(ajouterSoins, "comboBox");
-        comboBox.setSelectedItem("détartrage");
-
-        JComboBox<Integer> jourComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "jourComboBox");
-        jourComboBox.setSelectedItem(15);
-
-        JComboBox<Integer> moisComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "moisComboBox");
-        moisComboBox.setSelectedItem(8);
-
-        JComboBox<Integer> anneeComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "anneeComboBox");
-        anneeComboBox.setSelectedItem(2024);
-
-        JButton enregistrerButton = (JButton) getPrivateField(ajouterSoins, "enregistrerButton");
-        enregistrerButton.doClick();
-
-        // Verify the interaction with MongoDB
-        Document found = collection.find(new Document("nom", "Dupont")).first();
-        assertNotNull(found, "Patient 'Dupont' should be found");
-        assertEquals("Jean", found.getString("prenom"));
-        
-        List<Document> soinsList = (List<Document>) found.get("soins");
-        assertNotNull(soinsList);
-        assertEquals(1, soinsList.size());
-
-        Document soinDocument = soinsList.get(0);
-        assertEquals("détartrage", soinDocument.getString("soin"));
-        assertEquals("15/8/2024", soinDocument.getString("date"));
-    }
-
-    @Test
-    public void testEnregistrerButtonUpdateExistingPatient() {
-        // Setup existing patient
-        Document existingPatient = new Document("nom", "Dupont")
-                .append("prenom", "Jean")
-                .append("soins", List.of());
-        collection.insertOne(existingPatient);
-
-        // Configure the test data
-        ajouterSoins.nomText.setText("Dupont");
-        ajouterSoins.prenomText.setText("Jean");
-
-        JComboBox<String> comboBox = (JComboBox<String>) getPrivateField(ajouterSoins, "comboBox");
-        comboBox.setSelectedItem("plombage");
-
-        JComboBox<Integer> jourComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "jourComboBox");
-        jourComboBox.setSelectedItem(20);
-
-        JComboBox<Integer> moisComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "moisComboBox");
-        moisComboBox.setSelectedItem(7);
-
-        JComboBox<Integer> anneeComboBox = (JComboBox<Integer>) getPrivateField(ajouterSoins, "anneeComboBox");
-        anneeComboBox.setSelectedItem(2024);
-
-        JButton enregistrerButton = (JButton) getPrivateField(ajouterSoins, "enregistrerButton");
-        enregistrerButton.doClick();
-
-        // Verify the update in MongoDB
-        Document found = collection.find(new Document("nom", "Dupont")).first();
-        assertNotNull(found, "Patient 'Dupont' should be found");
-
-        List<Document> soinsList = (List<Document>) found.get("soins");
-        assertNotNull(soinsList);
-        assertEquals(1, soinsList.size());
-
-        Document soinDocument = soinsList.get(0);
-        assertEquals("plombage", soinDocument.getString("soin"));
-        assertEquals("20/7/2024", soinDocument.getString("date"));
-    }
-
-    private Object getPrivateField(Object object, String fieldName) {
-        try {
-            var field = object.getClass().getDeclaredField(fieldName);
-            field.setAccessible(true);
-            return field.get(object);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
