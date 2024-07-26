38e82913f8aad9a1fb70d10ec8f98aeb51f5e73f
asmabouziri <asmabouziri299@gmail.com>
2024-07-26 12:25:21 +0100

    26/07/2024*

--- src/test/java/SeConnecterTest.java
+++ src/test/java/SeConnecterTest.java
@@ -60,23 +60,6 @@ class SeConnecterTest {
 
         // Simulate button click
         seConnecter.btnNewButton.doClick();
-    }
-
-    @Test
-    public void testFailedLogin() {
-    	
-    	  // Add a user for testing
-        Document user = new Document("user", "testUser")
-                            .append("pwd", "testPass");
-        collection.insertOne(user);
-
-        
-        // Set invalid user credentials
-        seConnecter.txtEmail.setText("invalidUser");
-        seConnecter.textField.setText("invalidPass");
-
-        // Simulate button click
-        seConnecter.btnNewButton.doClick();
-
+ 
     }
 }
