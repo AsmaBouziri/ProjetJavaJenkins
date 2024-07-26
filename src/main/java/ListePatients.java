a8696ddda442eee0dc9a0b1556205096172f97f7
asmabouziri <asmabouziri299@gmail.com>
2024-07-26 10:12:34 +0100

    26/07/2024

--- src/main/java/ListePatients.java
+++ src/main/java/ListePatients.java
@@ -62,16 +62,16 @@ public class ListePatients extends JFrame {
         var panelButtons = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         panelButtons.setBackground(SystemColor.activeCaption);
 
-        var homeButton = new JButton("");
-        homeButton.setIcon(new ImageIcon(ListePatients.class.getResource("./images/home.png")));
-        panelButtons.add(homeButton);
-        homeButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                Acceuil acc = new Acceuil();
-                setVisible(false);
-                acc.setVisible(true);
-            }
-        });
+//        var homeButton = new JButton("");
+//        homeButton.setIcon(new ImageIcon(ListePatients.class.getResource("./images/home.png")));
+//        panelButtons.add(homeButton);
+//        homeButton.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                Acceuil acc = new Acceuil();
+//                setVisible(false);
+//                acc.setVisible(true);
+//            }
+//        });
 
         contentPane.add(panelButtons, BorderLayout.NORTH);
     }
