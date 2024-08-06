dea3be66e1cb94cb56443601c987dc1e138dccb4
asmabouziri <asmabouziri299@gmail.com>
2024-08-06 10:48:14 +0100

    05/08/2024

--- src/test/java/SeConnecterTest.java
+++ src/test/java/SeConnecterTest.java
@@ -1,70 +1,59 @@
-//package test.java;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import javax.swing.JOptionPane;
-//import javax.swing.JTextField;
-//import javax.swing.JButton;
-//import javax.swing.JFrame;
-//
-//import org.bson.Document;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.AfterEach;
-//
-//import com.mongodb.client.MongoCollection;
-//import com.mongodb.client.MongoDatabase;
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoClients;
-//import com.mongodb.client.MongoCursor;
-//
-//import main.java.MongoDBUtil;
-//import main.java.SeConnecter;
-//
-//class SeConnecterTest {
-//
-//    private SeConnecter seConnecter;
-//    private MongoClient mongoClient;
-//    private MongoDatabase database;
-//    private MongoCollection<Document> collection;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        // Set up MongoDB client and database
-//    	database = MongoDBUtil.getDatabase("CabinetDent");
-//        collection = database.getCollection("LogIn");
-//        
-//        // Initialize the SeConnecter instance
-//        seConnecter = new SeConnecter();
-//        seConnecter.setSize(800, 450);
-//        seConnecter.setVisible(true);
-//    }
-//
-//
-//    @Test
-//    public void testComponentsInitialization() {
-//        assertNotNull(seConnecter.txtEmail, "txtEmail should not be null");
-//        assertNotNull(seConnecter.textField, "textField should not be null");
-//        assertNotNull(seConnecter.btnSeConnecter, "btnNewButton should not be null");
-//    }
-//
-////    @Test
-////    public void testSuccessfulLogin() {
-////        // Add a user for testing
-////        Document user = new Document("user", "testUser")
-////                            .append("pwd", "testPass");
-////        collection.insertOne(user);
-////
-////        
-////        // Set user credentials
-////        seConnecter.txtEmail.setText("testUser");
-////        seConnecter.textField.setText("testPass");
-////
-////        // Simulate button click
-////        seConnecter.btnNewButton.doClick();
-////
-////
-////    }
-//
-//
-//}
+package test.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.bson.Document;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import main.java.MongoDBUtil;
+import main.java.SeConnecter;
+
+class SeConnecterTest {
+
+    private SeConnecter seConnecter;
+    private MongoDatabase database;
+    private MongoCollection<Document> collection;
+
+    @BeforeEach
+    public void setUp() {
+        // Set up MongoDB client and database
+    	database = MongoDBUtil.getDatabase("CabinetDent");
+        collection = database.getCollection("LogIn");
+        
+        // Initialize the SeConnecter instance
+        seConnecter = new SeConnecter();
+        seConnecter.setSize(800, 450);
+        seConnecter.setVisible(true);
+    }
+
+
+    @Test
+    public void testComponentsInitialization() {
+        assertNotNull(seConnecter.txtEmail, "txtEmail should not be null");
+        assertNotNull(seConnecter.textField, "textField should not be null");
+        assertNotNull(seConnecter.btnSeConnecter, "btnNewButton should not be null");
+    }
+
+    @Test
+    public void testSuccessfulLogin() {
+        // Add a user for testing
+        Document user = new Document("user", "testUser")
+                            .append("pwd", "testPass");
+        collection.insertOne(user);
+
+        
+        // Set user credentials
+        seConnecter.txtEmail.setText("testUser");
+        seConnecter.textField.setText("testPass");
+
+        // Simulate button click
+        seConnecter.btnSeConnecter.doClick();
+
+
+    }
+}
