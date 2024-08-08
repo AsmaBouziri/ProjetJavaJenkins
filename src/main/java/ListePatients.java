e2bd86fda33676abf1586bfb3652da0a5d92e104
asmabouziri <asmabouziri299@gmail.com>
2024-08-08 12:23:18 +0100

    08/08/2024

--- src/main/java/ListePatients.java
+++ src/main/java/ListePatients.java
@@ -18,6 +18,7 @@ public class ListePatients extends JFrame {
         ListePatients frame = new ListePatients();
         frame.setVisible(true);
         frame.setSize(800, 600);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
     public ListePatients() {
