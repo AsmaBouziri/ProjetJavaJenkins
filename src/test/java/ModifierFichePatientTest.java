00f6ee02c548d38941872e00ce64b6817f5483d0
asmabouziri <asmabouziri299@gmail.com>
2024-08-19 14:06:45 +0100

    19/08/2024

--- src/test/java/ModifierFichePatientTest.java
+++ src/test/java/ModifierFichePatientTest.java
@@ -11,8 +11,8 @@ import org.junit.jupiter.api.Test;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
